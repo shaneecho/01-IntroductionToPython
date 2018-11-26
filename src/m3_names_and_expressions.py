1a85837fd2e9a21391f5fa885b578bb050b793d1
yans2 <yans2@rose-hulman.edu>
2018-11-25 20:24:06 -0500

    m3

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -103,7 +103,7 @@ print(math)
 print('math')
 ###############################################################################
 #
-# TODO: 6.
+# Done: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
